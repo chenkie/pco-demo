c47c43030184768d6999e4c60bd6ac3a39f08419
Ryan Chenkie <ryanchenkie@gmail.com>
2017-07-28 18:40:50 -0400

    move access token to client

--- server.js
+++ server.js
@@ -10,8 +10,6 @@ const CLIENT_ID = 'fb493d88a959b9739c138854efc3c678b90ba1b324c249aa5db7e80bad45d
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = 'https://pco-demo.herokuapp.com';
 
-let accessToken = '';
-
 app.use(express.static(__dirname));
 
 app.post('/auth/exchange', (req, res) => {
@@ -23,23 +21,13 @@ app.post('/auth/exchange', (req, res) => {
     "client_secret": CLIENT_SECRET,
     "redirect_uri": REDIRECT_URI
   }).then(result => {
-    accessToken = result.data.access_token;
-    res.send('success');
+    const accessToken = result.data.access_token;
+    const expiresIn = result.data.expires_in;
+    res.send({ accessToken, expiresIn });
   }).catch(err => {
     console.log(err)
     res.send(err);
   });
-});
-
-app.get('/people', (req, res) => {
-  const headers = { Authorization: `Bearer ${accessToken}` };
-  console.log(headers);
-  axios.get('https://api.planningcenteronline.com/people/v2/people', { headers })
-    .then(result => {
-      res.send(result.data);
-    }).catch(error => {
-      res.send(error);
-    });
 });
 
 app.listen(process.env.PORT || 8080, () => {
