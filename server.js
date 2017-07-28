0cc93be3a962b95c1c2c9db4b1a466f07baa2b9f
Ryan Chenkie <ryanchenkie@gmail.com>
2017-07-28 14:31:43 -0400

    add dotenv

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const bodyParser = require('body-parser');
 const app = express();
+require('dotenv').config();
 
 app.use(bodyParser());
 
